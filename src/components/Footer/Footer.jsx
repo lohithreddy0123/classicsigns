1030a41209384a8cd6497cd28fdebe95931cfea6
lohithreddy0123 <lohithreddy0123@gmail.com>
2026-01-25 23:55:49 +0530

    footer ui is added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,60 @@
-import './footer.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './footer.css';
+import logo from '../../assets/images/logo.png';
 
 const Footer = () => {
   return (
-    <footer className="site-footer">
-      <p>© 2026 Classic Signs</p>
-    </footer>
-  )
-}
+    <footer className="footer">
+      <div className="footer-section footer-logo">
+        <Link to="/">
+          <img src={logo} alt="classicsigns Logo" className="logo" />
+        </Link>
+      </div>
 
-export default Footer
+      <div className="footer-container">
+        <div className="footer-section footer-links">
+          <h4>Quick Links</h4>
+          <ul className="footer-links-list">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About Us</Link></li>
+            <li><Link to="/services">Services</Link></li>
+            <li><Link to="/contact">Contact Us</Link></li>
+          </ul>
+
+        </div>
+
+        <div className="footer-section footer-legal">
+          <h4>Legal</h4>
+          <ul className="footer-links-list">
+            <li><Link to="/TermsAndConditions">Policies</Link></li>
+            <li><a href="#faq-section">FAQ</a></li>
+          </ul>
+
+        </div>
+
+        <div className="footer-section footer-social">
+          <h4>Follow Us</h4>
+          <div className="social-icons">
+            <a href="https://www.linkedin.com/company/vyrex-seo" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/ios-filled/30/ffffff/linkedin.png" alt="LinkedIn" />
+            </a>
+            <a href="https://x.com/VyrexOfficial" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/ios-filled/30/ffffff/twitter.png" alt="Twitter" />
+            </a>
+            <a href="https://www.facebook.com/share/u64nanas4ZY8icGJ/" target="_blank" rel="noopener noreferrer">
+              <img src="https://img.icons8.com/ios-filled/30/ffffff/facebook-new.png" alt="Facebook" />
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <hr className="footer-divider" />
+      <div className="footer-bottom">
+        <p>© 2026 Classic Porcelain Signs. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
