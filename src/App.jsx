64e43a05fec574e96a494d3f7bbb08ffafc5c929
Lohith Reddy <lohithreddy0123@gmail.com>
2026-04-22 14:50:41 +0530

    removed footer to test

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { lazy, Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
+// import Footer from "./components/Footer/Footer";
 import ScrollToTop from "./components/common/ScrollToTop";
 
 const Home     = lazy(() => import("./pages/Home/Home"));
@@ -25,35 +25,10 @@ function App() {
             <Route path="/contact" element={<Contact />} />
             <Route path="/blogs" element={<Blogs />} />
             <Route path="/blogs/:slug" element={<BlogPost />} />
-            <Route path="*" element={
-              <div style={{
-                minHeight: "80vh",
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                justifyContent: "center",
-                textAlign: "center",
-                padding: "20px"
-              }}>
-                <h1 style={{ fontSize: "48px", marginBottom: "10px" }}>404</h1>
-                <p style={{ fontSize: "18px", marginBottom: "20px" }}>
-                  Page not found
-                </p>
-                <a href="/" style={{
-                  padding: "10px 20px",
-                  background: "#000",
-                  color: "#fff",
-                  textDecoration: "none",
-                  borderRadius: "6px"
-                }}>
-                  Go Home
-                </a>
-              </div>
-            } />
           </Routes>
         </Suspense>
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 }
